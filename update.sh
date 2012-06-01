d3fc344f6e1a31aa60ab2d1a579fdffeb2eef457
Derick Rethans <github@derickrethans.nl>
2012-06-01 12:17:44 +0100

    Remove the startup bit of this.

--- update.sh
+++ update.sh
@@ -2,22 +2,15 @@
 
 export JAVACMD_OPTIONS="$JAVACMD_OPTIONS -Djava.net.preferIPv4Stack=true"
 
-echo "SETUP"
-mkdir -p backup
-mkdir -p dumps
-
-#wget http://download.geofabrik.de/osm/europe/great_britain.osm.pbf
-#../osmosis-0.39/bin/osmosis --rb great_britain.osm.pbf --bb left=-3.85 right=-3.70 top=55.72 bottom=55.60 --wx party.osm
-#../osmosis-0.39/bin/osmosis --rrii 
-# get state file from http://planet.openstreetmap.org/minute-replicate/
-#55.6798, -3.816, 55.652, -3.7486
-
 echo "UPDATE"
 
 ../osmosis-0.39/bin/osmosis --rri --simc --rx party.osm --ac --bb `cat config.txt | grep "bbox=" | cut -d "=" -f 2-` --wx party-new.osm || exit 4
 
 export TS=`cat state.txt | grep timestamp | sed 's/timestamp=//' | sed 's/\\\//g'`
 export NOW=`date +%Y%m%d-%H%M -d $TS`
+
+echo
+echo "Creating a backup"
 
 tar -cvjf backup/party-$NOW.tar.bz2 party.osm state.txt
 
