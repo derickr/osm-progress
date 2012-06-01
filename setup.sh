df1d0077d8d714b5ca7fd8209b951279eb0faa01
Derick Rethans <github@derickrethans.nl>
2012-06-01 12:09:47 +0100

    Fixed state file downloading instructions.

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ mkdir -p images
 
 wget http://download.geofabrik.de/osm/europe/great_britain.osm.pbf
 ../osmosis-0.39/bin/osmosis --rb great_britain.osm.pbf --bb `cat config.txt | grep "bbox=" | cut -d "=" -f 2-` --wx party.osm
+rm configuration.txt
 ../osmosis-0.39/bin/osmosis --rrii 
-# get state file from http://planet.openstreetmap.org/minute-replicate/
+# get state file from http://planet.openstreetmap.org/redaction-period/minute-replicate
 #55.6798, -3.816, 55.652, -3.7486
