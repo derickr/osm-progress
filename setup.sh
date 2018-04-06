b8a028a56cdfdc991c994fb2360f985cee67ac5d
Guillaume Rischard <github@stereo.lu>
2018-04-06 17:54:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ mkdir -p dumps
 mkdir -p gpx
 mkdir -p images
 
-#wget http://download.geofabrik.de/openstreetmap/europe/great-britain-latest.osm.pbf -O great-britain.osm.pbf
+#wget https://download.geofabrik.de/openstreetmap/europe/great-britain-latest.osm.pbf -O great-britain.osm.pbf
 #bzcat planet-091230.osm.bz2 | ../osmosis-latest/bin/osmosis --read-xml file=- --bb `cat config.txt | grep "bbox=" | cut -d "=" -f 2-` --lp --wx party.osm
 ../osmosis-latest/bin/osmosis --rbf workers=16 file=great-britain.osm.pbf --bb `cat config.txt | grep "bbox=" | cut -d "=" -f 2-` --lp --wx party.osm
 rm configuration.txt
 ../osmosis-latest/bin/osmosis --rrii 
-# get state file from http://planet.openstreetmap.org/replication-minute
+# get state file from https://planet.openstreetmap.org/replication-minute
 #55.6798, -3.816, 55.652, -3.7486
